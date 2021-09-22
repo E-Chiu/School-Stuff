d688112c0e80820f9201c92f729d6030360dd949
E-Chiu <31664636+E-Chiu@users.noreply.github.com>
2021-09-21 23:32:39 -0600

    source code does not match bytecode
    that bug has been bothering me all night i give up

--- Cmput301/MedBook/app/src/main/java/com/example/echiu_medbook/MainActivity.java
+++ Cmput301/MedBook/app/src/main/java/com/example/echiu_medbook/MainActivity.java
@@ -1,27 +1,39 @@
 package com.example.echiu_medbook;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     // declare variables
-    RecyclerView medView;
+    ListView medView;
     ArrayAdapter<Medicine> medAdapter;
-    ArrayList<Medicine> medList;
+    ArrayList<Medicine> medList; //list of medicines
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        medView = findViewById(R.id.med_View);
-        Medicine []medicines ={}; // list of medicines
-        medList = new ArrayAdapter<>(this, R.layout.content, medList);
+        // initialize listview of medicines
+        medView = findViewById(R.id.med_view);
+        medList = new ArrayList<>();
+        medAdapter = new ArrayAdapter<>(this, R.layout.content, medList);
         medView.setAdapter(medAdapter);
+    }
+
+    // function to add medicine, called by button press
+    public void addMedicine(View view) {
+        Intent intent = new Intent(this, AddMedicineActivity.class);
+        startActivity(intent);
     }
 }
