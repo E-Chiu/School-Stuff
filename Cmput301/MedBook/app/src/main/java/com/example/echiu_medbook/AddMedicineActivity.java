e43cd6fafdc7dbf681489bb637052637b9689807
E-Chiu <E-Chiu@users.noreply.github.com>
2021-09-23 12:51:19 -0600

    crunch time this weekend i guess

--- Cmput301/MedBook/app/src/main/java/com/example/echiu_medbook/AddMedicineActivity.java
+++ Cmput301/MedBook/app/src/main/java/com/example/echiu_medbook/AddMedicineActivity.java
@@ -3,12 +3,29 @@ package com.example.echiu_medbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddMedicineActivity extends AppCompatActivity {
+    String[] doseType = {"mg", "mcg", "drop"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_medicine);
+
+        Spinner doseType = (Spinner) findViewById(R.id.dose_unit_dropdown);
+        ArrayAdapter<String> units = new ArrayAdapter(this, android.R.layout.simple_spinner_item, doseType);
+
+    }
+
+    public void cancel(View view) { // finish activity without doing anything
+        super.finish();
+    }
+
+    public void add(View view) {
+    // first ensure that all entries are correct
+
     }
 }
