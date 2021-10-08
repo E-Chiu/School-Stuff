2fba22e3b27f86299d8cc2afa8ebd3f9f331ea89
E-Chiu <E-Chiu@users.noreply.github.com>
2021-10-07 19:07:10 -0600

    cmput301 lab 5 added

---
+++ Cmput301/CMPUT-301-CustomList/app/src/main/java/com/example/simpleparadox/listycity/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.simpleparadox.listycity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declare the variables so that you will be able to reference it later.
+    ListView cityList;
+    ArrayAdapter<City> cityAdapter;
+    ArrayList<City> cityDataList;
+
+    CustomList customList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String TAG = "Sample";
+        Button addCityButton;
+        final EditText addCityEditText;
+        final EditText addProvinceEditText;
+        FirebaseFirestore db;
+
+        cityList = findViewById(R.id.city_list);
+        addCityButton = findViewById(R.id.add_city_button);
+        addCityEditText = findViewById(R.id.add_city_field);
+        addProvinceEditText = findViewById(R.id.add_province_edit_text);
+
+        // Get a reference to the ListView and create an object for the city list
+        cityList = findViewById(R.id.city_list);
+        cityDataList = new ArrayList<>();
+        // Set the adapter for the ListView to the CustomAdapter that we created in Lab 3
+        cityAdapter = new CustomList(this, cityDataList);
+        cityList.setAdapter(cityAdapter);
+
+        // Access a Cloud Firestore instance from your Activity
+        db = FirebaseFirestore.getInstance();
+
+        // Get a top level reference to the collection
+        final CollectionReference collectionReference = db.collection("Cities");
+
+        addCityButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Retrieving the city name and the province name from the EditText fields
+                final String cityName = addCityEditText.getText().toString();
+                final String provinceName = addProvinceEditText.getText().toString();
+
+                HashMap<String, String> data = new HashMap<>();
+
+                if (cityName.length()>0 && provinceName.length()>0) {
+                    // If there’s some data in the EditText field, then we create a new key-value pair.
+                    data.put("Province Name", provinceName);
+
+                    // The set method sets a unique id for the document
+                    collectionReference
+                            .document(cityName)
+                            .set(data)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    // These are a method which gets executed when the task is succeeded
+                                    Log.d(TAG, "Data has been added successfully!");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // These are a method which gets executed if there’s any problem
+                                    Log.d(TAG, "Data could not be added!" + e.toString());
+                                }
+                            });
+                    // Setting the fields to null so that user can add a new city
+                    addCityEditText.setText("");
+                    addProvinceEditText.setText("");
+                }
+
+            }
+        });
+
+        cityDataList = new ArrayList<>();
+
+        cityAdapter = new CustomList(this, cityDataList);
+
+        cityList.setAdapter(cityAdapter);
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
+                    FirebaseFirestoreException error) {
+                cityDataList.clear();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+                {
+                    Log.d(TAG, String.valueOf(doc.getData().get("Province Name")));
+                    String city = doc.getId();
+                    String province = (String) doc.getData().get("Province Name");
+                    cityDataList.add(new City(city, province)); // Adding the cities and provinces from FireStore
+                }
+                cityAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
+            }
+        });
+
+//        dataList = new ArrayList<>();
+//        dataList.addAll(Arrays.asList(cities));
+//
+//        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+//
+//        cityList.setAdapter(cityAdapter);
+
+
+
+    }
+
+
+}
