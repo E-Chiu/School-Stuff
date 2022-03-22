1666c897e733c81e64de9a1e48cb2356206f0451
E-Chiu <31664636+E-Chiu@users.noreply.github.com>
2022-03-22 09:00:40 -0600

    assignment 3 is a yikes

--- cmput325/1621615.pl
+++ cmput325/1621615.pl
@@ -1,5 +1,5 @@
-% q1: the setDifference function takes 2 lists S1, S2 and returns the difference of the two lists in S3
-% the code for the member function is taken from class slides
+% q1: the setDifference predicate takes 2 lists S1, S2 and returns the difference of the two lists in S3
+% the code for the member predicate is taken from class slides
 member(E, [E | _]).
 member(E, [_ | L]) :- member(E, L).
 
@@ -15,14 +15,14 @@ setDifference([A|S1], S2, [A|S3]) :-
     setDifference(S1, S2, S3).
 
 
-% q2: the swap function takes a list and every pair of elements in the list are swapped
+% q2: the swap predicate takes a list and every pair of elements in the list are swapped
 swap([], []).
 swap([A], [A]).
 swap([A, B|L], [B, A| R]) :- 
     swap(L, R).
 
-% q3: the filter function takes a possibly nested list of numbers and returns a list of numbers that satisfy specifications
-% the flatten function is taken from the classs slides
+% q3: the filter predicate takes a possibly nested list of numbers and returns a list of numbers that satisfy specifications
+% the flatten predicate is taken from the classs slides
 flatten([],[]).
 flatten([A|L],[A|L1]) :-
  xatom(A), flatten(L,L1).
@@ -56,31 +56,43 @@ filterHelper([A|L], lessThan, N, L1) :- % go to next when isn't greater than
     A >= N,
     filterHelper(L, lessThan, N, L1).
 
-% q4: the countAll function counts the occurance of every atom in the list and sorts them
+% q4: the countAll predicate counts the occurance of every atom in the list and sorts them
 % modMember is a modified member from the class slides that checks within nested lists
-modMember(E, [[E | _] | _]).
-modMember(E, [_ | L]) :- 
-    modMember(E, L).
+countAll(L, N) :-
+    countAllHelper(L, [], N).
 
-countAll([],[]).
-countAll([A|L], N) :-
-    var(N),
-    countAll(L, [[A, 1] |N]).
-countAll([A|L], [[A, 1] | N]) :- % create new if isnt a member
-    \+ modMember(A, N),
-    countAll(L, N).
-/*countAll([A|L], N) :- % increment counter
-    countAllHelper(A, N, N1),
-    countAll(L, N1).
-*/
-countAllHelper(_, [], []).
-countAllHelper(A, [[B | D] | C], N1) :- % if found then increment
+countAllHelper([], _, _).
+countAllHelper([A | L], [], L1) :- % if not found add to list
+    countAllHelper(L, [[A, 1] | L1], [[A, 1] | L1]).
+countAllHelper([A | L], [[B | [C | _]] | L1], [[B , C1] | L2]) :- % if found then increment
     A = B,
-    D1 is D + 1,
-    countAllHelper(A, C, [[_ | D1] | N1]).
-countAllHelper(A, [[B | _] | C], [_ | N1]) :- % if not found keep looking
-    A =\= B,
-    countAllHelper(A, C, N1).
+    C1 is C + 1,
+    countAllHelper(L,  [[B , C1] | L2], L2). % update list
+countAllHelper([A | L], [_ | L1], L2) :- % if not found check next
+    countAllHelper([A | L], L1, L2).
 
-% q5: the sub function takes a list of pairs [xi, ei] and searches through a list, replacing every occurance of xi with ei in the list
-sub(L, S, L1).
+% q5: the sub predicate takes a list of pairs [xi, ei] and searches through a list, replacing every occurance of xi with ei in the list
+% NOTE I WAS UNABLE TO REPRODUCE THE NESTED STRUCTURE WHEN I SUBSTITUTE, BUT FLATTEN WAS NOT USED, I ENTERED THE NESTED
+% LISTS PROPERLY.
+sub([], _, []).
+sub([A | L], S, [B | L1]) :- % if atom check if need to be replaced
+    atom(A),
+    sub(L, S, L1),
+    subHelper(A, S, B).
+sub([A | L], S, L1) :- % if nested search both parts
+    \+ atom(A),
+    sub(A, S, L2),
+    sub(L, S, L3),
+    append(L2, L3, L1).
+
+subHelper(A, [], A). % if sub list empty add to list and go back
+subHelper(A, [[B | [C | _]] | _], C) :- % if found in sub list substitute
+    A = B.
+subHelper(A, [[B | _] | S], C) :- % otherwise keep searching list
+    \+ A = B,
+    subHelper(A, S, C).
+
+% q6: the clique predicate 
+% clique(L).
+
+% q7:
