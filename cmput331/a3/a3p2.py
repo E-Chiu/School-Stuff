dd457082df150ed12af3240baa3d30dee2f1bb3f
Ethan Chiu <31664636+E-Chiu@users.noreply.github.com>
2023-09-26 21:06:56 -0600

    add a3 files

---
+++ cmput331/a3/a3p2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+#---------------------------------------------------------------
+#
+# CMPUT 331 Student Submission License
+# Version 1.0
+# Copyright 2023 <<Insert your name here>>
+#
+# Redistribution is forbidden in all circumstances. Use of this software
+# without explicit authorization from the author is prohibited.
+#
+# This software was produced as a solution for an assignment in the course
+# CMPUT 331 - Computational Cryptography at the University of
+# Alberta, Canada. This solution is confidential and remains confidential 
+# after it is submitted for grading.
+#
+# Copying any part of this solution without including this copyright notice
+# is illegal.
+#
+# If any portion of this software is included in a solution submitted for
+# grading at an educational institution, the submitter will be subject to
+# the sanctions for plagiarism at that institution.
+#
+# If this software is found in any public website or public repository, the
+# person finding it is kindly requested to immediately report, including 
+# the URL or other repository locating information, to the following email
+# address:
+#
+#          gkondrak <at> ualberta.ca
+#
+#---------------------------------------------------------------
+
+"""
+CMPUT 331 Assignment 3 Student Solution
+September 2023
+Author: <Your name here>
+"""
+
+def random_generator(a, b, c, m, r0, r1, n):
+    pass
+
+
+
+def test():
+    assert random_generator(3, 5, 9, 16, 11, 6, 3) == [2, 13, 10]
+    assert random_generator(22695477, 77557187, 259336153, 9672485827, 42, 51, 8) == [4674207334, 3722211255, 3589660660, 1628254817, 8758883504, 7165043537, 4950370481, 2261710858]
+    assert random_generator(2**31-5, 743, 549, 2**64 - 1, 97, 101, 8) == [216895920563, 4611839799731136226, 4610466323469147181, 6003130118321022275, 14149176448272843437, 14229211546334517160, 10244020959373064815, 2515976774685790769]
+    assert random_generator(1128889, 1023, 511, 222334565193649, 65535, 329, 8) == [438447297, 50289200612813, 17962583104439, 47361932650166, 159841610077391, 19587857129781, 111993173627854, 7567964632208]
+
+from sys import flags
+
+if __name__ == "__main__" and not flags.interactive:
+    test()
