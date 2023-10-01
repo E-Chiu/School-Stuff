e4851dcf7460cc5b559aadb874b26a8212c7d345
E-Chiu <31664636+E-Chiu@users.noreply.github.com>
2023-10-01 17:48:55 -0600

    a3 completed

---
+++ cmput331/a3/README.txt
@@ -0,0 +1,5 @@
+Chiu, Ethan Cmput 331 Assignment 3
+
+modular inverse for part 3 was calculated using https://www.dcode.fr/modular-inverse
+
+function used to find a modular inverse in part 4 was obtained from https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
--- cmput331/a3/a3p4.py
+++ cmput331/a3/a3p4.py
@@ -4,7 +4,7 @@
 #
 # CMPUT 331 Student Submission License
 # Version 1.0
-# Copyright 2023 <<Insert your name here>>
+# Copyright 2023 <Ethan Chiu>
 #
 # Redistribution is forbidden in all circumstances. Use of this software
 # without explicit authorization from the author is prohibited.
@@ -33,19 +33,68 @@
 """
 CMPUT 331 Assignment 3 Student Solution
 September 2023
-Author: <Your name here>
+Author: Ethan Chiu
 """
 
 def crack_rng(m, sequence):
     r2, r3, r4, r5, r6 = tuple(sequence)
-    pass
+    # using equations:
+    # eq1: R6 = aR5 + bR4 + c (mod m)
+    # eq2: R5 = aR4 + bR3 + c (mod m)
+    # eq3: R4 = aR3 + bR2 + c (mod m)
+
+    # first subtract the variables from each other to remove c
+    # eq4 = eq1 - eq3
+    eq4 = r6 - r4
+    aCoeff4 = r5 - r3
+    bCoeff4 = r4 - r2
+    # eq5 = eq2 - eq3
+    eq5 = r5 - r4
+    aCoeff5 = r4 - r3
+    bCoeff5 = r3 - r2
+
+    # since the denominator can be crossed out we can cross multiply in advance to avoid working with decimals
+    # eq6 = eq4 * bCoeff5
+    eq6 = eq4 * bCoeff5
+    aCoeff6 = aCoeff4 * bCoeff5
+    # eq7 = eq5 * beCoeff4
+    eq7 = eq5 * bCoeff4
+    aCoeff7 = aCoeff5 * bCoeff4
+
+    # subtract the equations to single out b
+    # eq 8 = eq6 - eq 7
+    eq8 = eq6 - eq7
+    aCoeff8 = aCoeff6 - aCoeff7
+    
+    # find the modular inverse of a
+    aInv = pow(aCoeff8, -1, m)
+    # find a
+    a = (eq8 * aInv) % m
+
+    # plug back into the eqation to find b
+    # plug into eq4
+    eq4 = eq4 - (aCoeff4 * a)
+
+    # find inverse of b
+    bInv = pow(bCoeff4, -1, m)
+    #find b
+    b = (eq4 * bInv) % m
+
+    # plug back into the eqation to find c
+    # plug into eq1
+    eq1 = r6 - (a * r5 + b * r4)
+    # find c
+    c = eq1 % m
+
+    # return the vars
+    return [a, b, c]
 
 def test():
     assert crack_rng(17, [14, 13, 16, 3, 13]) == [3, 5, 9]
     assert crack_rng(9672485827, [4674207334, 3722211255, 3589660660, 1628254817, 8758883504]) == [22695477, 77557187, 259336153]
     assert crack_rng(101, [0, 91, 84, 16, 7]) == [29, 37, 71]
     assert crack_rng(222334565193649,[438447297,50289200612813,17962583104439,47361932650166,159841610077391]) == [1128889, 1023, 511]
-    
+
 from sys import flags
 
 if __name__ == "__main__" and not flags.interactive:
